0c1ae7bddf034e9ec49993076d806aaad8cf7d83
DemuraAIdev <cyberaioff@gmail.com>
2022-02-27 23:32:05 +0700

    firs

---
+++ constants/data.js
@@ -0,0 +1,95 @@
+const userData = {
+  githubUsername: "DemuraAIdev",
+  name: "Abdul Vaiz Vahry Iskandar",
+  designation: "Developer",
+  avatarUrl: "/Moon.jpg",
+  email: "demura@cintanime.xyz",
+  phone: "+62 895621990556",
+  address: "indonesia, Bengkulu, Kota Bengkulu.",
+  projects: [
+    {
+      title: "DotBot Project",
+      link: "https://github.com/DemuraAIdev/DotBot",
+      imgUrl: "/dotbot.png",
+    },
+    {
+      title: "DRM Framework",
+      link: "https://github.com/DemuraAIdev/drm_framework",
+      imgUrl: "/drm.jpg",
+    },
+    {
+      title: "CA",
+      link: "https://cintanime.xyz",
+      imgUrl: "/ca.png",
+    },
+    {
+      title: "Dencrypt",
+      link: "https://github.com/DemuraAIdev/dencrypt",
+      imgUrl: "/den.png",
+    }
+  ],
+  about: {
+    title:
+      "I`am a developer Javascript",
+    description: [
+      `I am a website developer, bot, graphic designer, and nft artist. I learned to make a website from 2019 at that time I learned basic html after 3 months I learned PHP to make my own framework but failed, at that time I found a chat platform called discord I looked for friends to help me but no one uses php they use nodejs , at that time 2020 i know nodejs`,
+      `in 2020 I tried to make a discord bot called INTI and I continue to learn and make another bot called DotBot. At that time I met a friend with the username Zeck and he invited me to create an anime fanspage called Cinta Anime.ID there were admins named Ray, Knightio, Leonz, Zeck, and I`,
+      ``,
+    ],
+    currentProject: "CA",
+    currentProjectUrl: "https://cintanime.xyz",
+  },
+  experience: [
+    {
+      title: "DotBot",
+      company: "JS",
+      year: "2021",
+      companyLink: "https://github.com/DemuraAIdev/DotBot",
+      desc: "i created a open source bot for discord with discord.js",
+    },
+    {
+      title: "CA(CintaAnime)",
+      company: "NEWS",
+      year: "2020",
+      companyLink: "https://cintanime.xyz",
+      desc: "i created a `CintaAnime` anime news called `CA`and instagram @cintanime.id.",
+    },
+    {
+      title: "Simple DIscord Bot",
+      company: "JS ",
+      year: "2020",
+      companyLink: "---",
+      desc: "Developed a discord bot that can be used to weather, time, date, and more.",
+    },
+    {
+      title: "Nodejs HelloWorld",
+      company: "JS",
+      year: "2020",
+      companyLink: "---",
+      desc: "A simple Nodejs application that prints 'Hello World' to the console.",
+    },
+    {
+      title: "First Website",
+      company: "HTML BOOTSTRAP JS.",
+      year: "2019",
+      companyLink: "--",
+      desc: "This is my first website. It's a simple website that I built using HTML, CSS, Bootstrap, Javascript and Jquery.",
+    },
+    {
+      title: "My First Hello World",
+      company: "HTML.",
+      year: "2019",
+      companyLink: "-----",
+      desc: "My first website hello world using html and js. it's pretty simple and it's awesome.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/cintanime.id",
+    twitter: "https://twitter.com/Abdulvaiz2",
+    github: "https://github.com/DemuraAIdev",
+  },
+};
+
+export default userData;
